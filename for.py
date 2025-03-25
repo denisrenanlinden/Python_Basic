0fdf9dd85fcf712f5fc07617e1bdba747ab8cda7
denisrenanlinden <88343413+denisrenanlinden@users.noreply.github.com>
2025-03-24 22:15:27 -0300

    input

--- for.py
+++ for.py
@@ -1,7 +1,4 @@
-#For in Python Language
+# For in Python Language
 
 for variavel_aqui in range(5):
     print(variavel_aqui)
-
-
-    
---
+++ input.py
@@ -0,0 +1,13 @@
+# Utilizando o Input com Python
+
+nome_do_filme = input("Nome do Filme: ")
+ano_lancamento = input("Ano de Lançamento: ")
+nota_do_filme = input("Nota do Filme: ")
+streaming_disponivel = input("Streaming Disponivel: ")
+multiplataforma = input("Multiplataforma: ")
+
+print(nome_do_filme)
+print(ano_lancamento)
+print(nota_do_filme)
+print(streaming_disponivel)
+print(multiplataforma)
