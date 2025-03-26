28ad29476848306856fccf39c267e245acff6280
denisrenanlinden <88343413+denisrenanlinden@users.noreply.github.com>
2025-03-26 00:37:52 -0300

    Update operadores.py

--- PY/operadores.py
+++ PY/operadores.py
@@ -11,6 +11,13 @@ dividir = numero1 / numero2
 dividir_inteiro = numero1 // numero2
 resto = numero1 % numero2
 potencia = numero1 ** numero2
+maior = numero1 > numero2
+menor = numero1 < numero2
+igual = numero1 == numero2
+diferente = numero1 != numero2
+maior_ou_igual = numero1 >= numero2
+menor_ou_igual = numero1 <= numero2
+
 
 print("================================================================")
 print(f'Soma: {somar}')
@@ -20,4 +27,10 @@ print(f'Divisão: {dividir}')
 print(f'Divisão inteira: {dividir_inteiro}')
 print(f'Resto da divisão de {numero1} por {numero2} é: {resto}')
 print(f'Potência do numero {numero1} por {numero2} é: {potencia}')
+print(f'O numero {numero1} é maior que o numero {numero2}? {maior}')
+print(f'O numero {numero1} é menor que o numero {numero2}? {menor}')
+print(f'O numero {numero1} é igual ao numero {numero2}? {igual}')
+print(f'O numero {numero1} é diferente do numero {numero2}? {diferente}')
+print(f'O numero {numero1} é maior que {numero2}? {maior_ou_igual}')
+print(f'O numero {numero1} é menor ou igual a {numero2}? {menor_ou_igual}')
 print("================================================================")
