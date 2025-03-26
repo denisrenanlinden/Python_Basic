3e7f96e6592f0ad14a31597206c155a73d2f2f9a
denisrenanlinden <88343413+denisrenanlinden@users.noreply.github.com>
2025-03-26 02:57:16 -0300

    Update dicionarios.py

---
+++ PY/dicionarios.py
@@ -0,0 +1,10 @@
+# Dicionário no Python (Muito parecido com um JSON)
+
+
+lista_de_alunos = {
+    'nome': 'Matheus',
+    'idade': 25,
+    'altura': 1.75,
+    'peso': 80,
+    'sexo': ('Masculino', 'Feminino', 'Outros'),
+}
