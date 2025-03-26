9a857452cee5e15cf0defb11b7bf25861279f9b1
denisrenanlinden <88343413+denisrenanlinden@users.noreply.github.com>
2025-03-25 22:14:49 -0300

    Create input3.py

---
+++ PY/input3.py
@@ -0,0 +1,21 @@
+# Utilizando o Input com Python
+
+nome_do_filme = input("Nome do Filme: ")
+ano_lancamento = int(input("Ano de Lançamento: "))
+nota_do_filme = float(input("Nota do Filme: "))
+streaming_disponivel = input("Streaming Disponivel: ")
+
+# Outra maneira de Printar no Python
+print("\n\n")
+print(f"Nome do Filme: {nome_do_filme}")
+print(f"Ano de Lançamento: {ano_lancamento}")
+print(f"Nota do Filme: {nota_do_filme}")
+print(f"Streaming Disponível: {streaming_disponivel}")
+
+
+# Mais uma maneira de Printar no Python
+print("\n\n")
+print(f"Nome do Filme: {nome_do_filme} \n"
+      f"Ano de Lançamento: {ano_lancamento} \n"
+      f"Nota do Filme: {nota_do_filme} \n"
+      f"Streaming Disponível: {streaming_disponivel} \n")
