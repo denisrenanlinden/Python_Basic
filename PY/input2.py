0078b35f687311cb77ebc1b61899b5f40d30461c
denisrenanlinden <88343413+denisrenanlinden@users.noreply.github.com>
2025-03-25 21:15:45 -0300

    Update input2.py

--- PY/input2.py
+++ PY/input2.py
@@ -4,7 +4,7 @@ nome_do_filme = input("Nome do Filme: ")
 ano_lancamento = int(input("Ano de Lançamento: "))
 nota_do_filme = float(input("Nota do Filme: "))
 streaming_disponivel = input("Streaming Disponivel: ")
-#multiplataforma = bool(input("Multiplataforma: "))
+# multiplataforma = bool(input("Multiplataforma: ")) Maneira incorreta de tratar Boleano
 
 print("\n")
 print("Exibindo os Tipos de Dados: \n")
@@ -12,7 +12,7 @@ print(type(nome_do_filme))
 print(type(ano_lancamento))
 print(type(nota_do_filme))
 print(type(streaming_disponivel))
-#print(type(multiplataforma))
+# print(type(multiplataforma)) Não adianta imprimir esse Boleano, sempre terá True no resultado
 
 print("\n")
 print("Exibindo os Dados: \n")
@@ -20,7 +20,7 @@ print("Nome do Filme:", nome_do_filme)
 print("Ano de Lançamento:", ano_lancamento)
 print("Nota do Filme:", nota_do_filme)
 print("Streaming Disponível:", streaming_disponivel)
-#print("Multiplataforma:", multiplataforma)
+# print("Multiplataforma:", multiplataforma) Vai imprimir sempre True
 
 # Recebendo um valor booleano corretamente
 valor_booleano = input("Digite 'True' ou 'False': ").strip().capitalize()
